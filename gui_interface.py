df42b90f04eae7af9ddaafbbf948d6d123d06b27
USER <julia.makareva@gmail.com>
2025-03-02 17:44:10 +0100

    File picker / files & directory

--- gui_interface.py
+++ gui_interface.py
@@ -27,7 +27,56 @@ def main(page: ft.Page):
         text_align=ft.TextAlign.CENTER
     )
 
+# selected file or folder
+    selected_source = Text("")
+    dst_path = Text("")
+    output_text = ft.Text("", color="red")
 
+    # pattern for a search function ( filter field)
+    search_pattern_input = ft.TextField(hint_text="Enter search pattern", width=200, visible=False)
 
+    def pick_files_result(e: FilePickerResultEvent):
+        if e.files:
+            selected_source.value = e.files[0].path
+            selected_source.update()
+
+            page.update()
+
+    def pick_directory_result(e: FilePickerResultEvent):
+        if e.path:
+            selected_source.value = e.path
+            selected_source.update()
+
+            page.update()
+
+    def pick_destination_result(e: FilePickerResultEvent):
+        if e.path:
+            dst_path.value = e.path
+            dst_path.update()
+            page.update()
+
+    pick_files_dialog = FilePicker(on_result=pick_files_result)
+    pick_directory_dialog = FilePicker(on_result=pick_directory_result)
+    pick_destination_dialog = FilePicker(on_result=pick_destination_result)
+    page.overlay.extend([pick_files_dialog, pick_directory_dialog, pick_destination_dialog])
+
+    page.add(
+        ft.Column([
+            image,
+            title,
+            Row([
+                ElevatedButton("Pick File", bgcolor="#36454F", icon=icons.UPLOAD_FILE,
+                               on_click=lambda _: pick_files_dialog.pick_files()),  # Bouton pour choisir un fichier
+                ElevatedButton("Pick Directory", bgcolor="#36454F", icon=icons.FOLDER_OPEN,
+                               on_click=lambda _: pick_directory_dialog.get_directory_path()),
+                # Bouton pour choisir un répertoire
+            ], alignment=ft.MainAxisAlignment.CENTER),  # Centrer les boutons dans la ligne
+
+        ],
+            expand=True,  # Permet à la colonne d'occuper tout l'espace disponible
+            alignment=ft.MainAxisAlignment.CENTER,  # Centrer le contenu de la colonne verticalement
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER  # Centrer horizontalement
+        )
+ )
 
 ft.app(target=main)
