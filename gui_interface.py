e93738c2f4f57f0c8209eeec2207873b82324562
USER <julia.makareva@gmail.com>
2025-03-02 17:28:40 +0100

    Adding an image to our file manager + def main

--- gui_interface.py
+++ gui_interface.py
@@ -1,5 +1,33 @@
 import flet as ft
+from flet import (
+    ElevatedButton,
+    FilePicker,
+    FilePickerResultEvent,
+    Row,
+    Text,
+    icons,
+)
 
 from manager import copy, delete, move_file, search, count_files, get_size, analyze, organize
 
+def main(page: ft.Page):
+    page.title = "File Manager"
+    image_path = "Smartphone_icon_17_File_Manager-512.webp"
+    image = ft.Image(src=image_path, width=150, height=150)
+    page.window_width = 600
+    page.window_height = 700
+    page.bgcolor = "white"
 
+    title = ft.Text(
+        value="Welcome to the File Manager!",
+        color="#36454F",
+        size=28,
+        italic=True,
+        weight=ft.FontWeight.BOLD,
+        text_align=ft.TextAlign.CENTER
+    )
+
+
+
+
+ft.app(target=main)
